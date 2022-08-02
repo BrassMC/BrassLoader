8f7d6c9af4fd24b739f65b73d996adeda83efc59
DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2022-08-02 13:40:37 +0100

    Add MC Mod Launcher

--- src/main/java/io/github/brassmc/brassloader/BrassLauncher.java
+++ src/main/java/io/github/brassmc/brassloader/BrassLauncher.java
@@ -1,7 +1,9 @@
 package io.github.brassmc.brassloader;
 
+import cpw.mods.modlauncher.Launcher;
+
 public class BrassLauncher {
     public static void main(String[] args) {
-
+        Launcher.main(args);
     }
 }
