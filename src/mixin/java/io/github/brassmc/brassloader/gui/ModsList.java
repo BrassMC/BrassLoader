a936c772f2be28cc179d997fe4ceddeadb946f77
DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2022-08-06 14:10:00 +0100

    Attempt ModsList

---
+++ src/mixin/java/io/github/brassmc/brassloader/gui/ModsList.java
@@ -0,0 +1,123 @@
+package io.github.brassmc.brassloader.gui;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import io.github.brassmc.brassloader.mods.ModSummary;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.ObjectSelectionList;
+import net.minecraft.network.chat.Component;
+
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+
+public class ModsList extends ObjectSelectionList<ModsList.ModListEntry> {
+    private final ModsListScreen screen;
+    private String filter = "";
+    @Nullable
+    private List<ModSummary> currentlyDisplayed;
+    private CompletableFuture<List<ModSummary>> pending;
+
+    public ModsList(ModsListScreen screen,  Minecraft minecraft, int xPos, int yPos, int width, int height, int itemHeight, @Nullable ModsList modsList) {
+        super(minecraft, xPos, yPos, width, height, itemHeight);
+        this.screen = screen;
+
+        if(modsList != null) {
+            this.pending = modsList.pending;
+        } else {
+            this.pending = loadMods();
+        }
+
+        handleNewMods(pollMods());
+    }
+
+    @Nullable
+    private List<ModSummary> pollMods() {
+        try {
+            return this.pending.getNow(null);
+        } catch (CancellationException | CompletionException exception) {
+            return null;
+        }
+    }
+
+    // TODO: Get from mod discovery
+    private CompletableFuture<List<ModSummary>> loadMods() {
+        return CompletableFuture.completedFuture(List.of(
+                new ModSummary("BeansMod", null, "Beans"),
+                new ModSummary("PeasMod", null, "Peas")
+        ));
+    }
+
+    private void handleNewMods(@Nullable List<ModSummary> modSummaryList) {
+        if(modSummaryList == null) {
+            fillLoadingMods();
+        } else {
+            fillMods(this.filter, modSummaryList);
+        }
+    }
+
+    private void fillLoadingMods() {
+        clearEntries();
+        notifyListUpdated();
+    }
+
+    public void updateFilter(String str) {
+        if(this.currentlyDisplayed != null && !str.equals(this.filter)) {
+            this.fillMods(str, this.currentlyDisplayed);
+        }
+
+        this.filter = str;
+    }
+
+    private void fillMods(String filter, List<ModSummary> currentlyDisplayed) {
+        this.clearEntries();
+        filter = filter.toLowerCase(Locale.ROOT);
+
+        for (ModSummary modSummary : currentlyDisplayed) {
+            if(matchesFilter(filter, modSummary)) {
+                addEntry(new ModListEntry(this, modSummary));
+            }
+        }
+
+        notifyListUpdated();
+    }
+
+    private boolean matchesFilter(String filter,  ModSummary modSummary) {
+        return modSummary.getName().toLowerCase(Locale.ROOT).contains(filter);
+    }
+
+    private void notifyListUpdated() {
+        this.screen.triggerImmediateNarration(true);
+    }
+
+    public static final class ModListEntry extends ObjectSelectionList.Entry<ModsList.ModListEntry> implements AutoCloseable {
+        private final Minecraft minecraft;
+        private final ModsListScreen screen;
+        private final ModSummary summary;
+
+        public ModListEntry(ModsList list,  ModSummary summary) {
+            this.minecraft = list.minecraft;
+            this.screen = list.screen;
+            this.summary = summary;
+        }
+
+        @Override
+        public Component getNarration() {
+            return Component.translatable("brass.narration.modList.entry", this.summary.getName());
+        }
+
+        @Override
+        public void render(PoseStack poseStack, int i, int i1, int i2, int i3, int i4, int i5, int i6, boolean b, float v) {
+            this.minecraft.font.draw(poseStack, this.summary.getName(), i + 20, i1 + 20, 0x404040);
+        }
+
+        public boolean isSelectable() {
+            return !summary.isDisabled();
+        }
+
+        public void close() {
+        }
+    }
+}
--- src/mixin/java/io/github/brassmc/brassloader/mixin/TitleScreenMixin.java
+++ src/mixin/java/io/github/brassmc/brassloader/mixin/TitleScreenMixin.java
@@ -1,8 +1,11 @@
 package io.github.brassmc.brassloader.mixin;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import io.github.brassmc.brassloader.gui.ModsListScreen;
 import net.minecraft.SharedConstants;
 import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.network.chat.Component;
@@ -23,5 +26,19 @@ public class TitleScreenMixin extends Screen {
     private void brass$changeText(PoseStack matrixStack, Font font, String s, int i, int j, int k) {
         String text = "Minecraft " + SharedConstants.VERSION_STRING + " / Brass (Modded)";
         drawString(matrixStack, this.font, text, 2, this.height - 10, 16777215);
+    }
+
+    @Redirect(
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/TitleScreen;addRenderableWidget(Lnet/minecraft/client/gui/components/events/GuiEventListener;)Lnet/minecraft/client/gui/components/events/GuiEventListener;", ordinal = 2),
+            method = "createNormalMenuOptions"
+    )
+    private GuiEventListener brass$addModsButton(TitleScreen instance, GuiEventListener guiEventListener) {
+        Button button = (Button) guiEventListener;
+        addRenderableWidget(new Button(button.x, button.y, button.getWidth() / 2 - 2, button.getHeight(),
+                Component.translatable("brass.menu.mods"),
+                btn -> this.minecraft.setScreen(new ModsListScreen(this))));
+        button.x += button.getWidth() / 2 + 2;
+        button.setWidth(button.getWidth() / 2);
+        return addRenderableWidget(button);
     }
 }
