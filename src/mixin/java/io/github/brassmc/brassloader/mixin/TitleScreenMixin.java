7e7a2626c2c17c4761ca7bbf3c0117f32949d6a1
AG6 <60290746+AG6Dev@users.noreply.github.com>
2022-08-06 13:39:54 +0100

    Changing the TitleScreenMixin to a ModifyVar

--- src/mixin/java/io/github/brassmc/brassloader/mixin/TitleScreenMixin.java
+++ src/mixin/java/io/github/brassmc/brassloader/mixin/TitleScreenMixin.java
@@ -1,14 +1,12 @@
 package io.github.brassmc.brassloader.mixin;
 
-import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.SharedConstants;
-import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin extends Screen {
@@ -16,12 +14,8 @@ public class TitleScreenMixin extends Screen {
         super($$0);
     }
 
-    @Redirect(
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/TitleScreen;drawString(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/gui/Font;Ljava/lang/String;III)V"),
-            method = "render"
-    )
-    private void brass$changeText(PoseStack matrixStack, Font font, String s, int i, int j, int k) {
-        String text = "Minecraft " + SharedConstants.VERSION_STRING + " / Brass (Modded)";
-        drawString(matrixStack, this.font, text, 2, this.height - 10, 16777215);
+    @ModifyVariable(at = @At("STORE"), method = "render(Lcom/mojang/blaze3d/vertex/PoseStack;IIF)V")
+    private String brass$changeTitle(String value) {
+        return "Minecraft " + SharedConstants.getCurrentVersion().getName() + " / Brass (Modded)";
     }
 }
