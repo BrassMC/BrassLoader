df9f57179c70186421ce0fd7f07856af30e24f76
DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2022-08-06 14:15:56 +0100

    Update TitleScreenMixin.java

--- src/mixin/java/io/github/brassmc/brassloader/mixin/TitleScreenMixin.java
+++ src/mixin/java/io/github/brassmc/brassloader/mixin/TitleScreenMixin.java
@@ -12,6 +12,7 @@ import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin extends Screen {
