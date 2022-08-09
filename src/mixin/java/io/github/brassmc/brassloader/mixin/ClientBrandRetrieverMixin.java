dde9aa16c4fbfa44b133d5a84cc17f653a1cc2ef
Affehund <42092761+Affehund@users.noreply.github.com>
2022-08-10 00:25:36 +0200

    Adjust debug overlay to brass instead of vanilla (#5)
    * adjust debug overlay
    
    * apply requested changes

---
+++ src/mixin/java/io/github/brassmc/brassloader/mixin/ClientBrandRetrieverMixin.java
@@ -0,0 +1,15 @@
+package io.github.brassmc.brassloader.mixin;
+
+import net.minecraft.client.ClientBrandRetriever;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(ClientBrandRetriever.class)
+public class ClientBrandRetrieverMixin {
+
+    @ModifyConstant(method = "getClientModName", constant = @Constant(stringValue = "vanilla"))
+    private static String brass$modifyClientBrandName(String original) {
+        return "brass";
+    }
+}
---
+++ src/mixin/java/io/github/brassmc/brassloader/mixin/MinecraftServerMixin.java
@@ -0,0 +1,15 @@
+package io.github.brassmc.brassloader.mixin;
+
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(MinecraftServer.class)
+public class MinecraftServerMixin {
+
+    @ModifyConstant(method = "getServerModName", constant = @Constant(stringValue = "vanilla"))
+    public String brass$modifyServerBrandName(String original) {
+        return "brass";
+    }
+}
